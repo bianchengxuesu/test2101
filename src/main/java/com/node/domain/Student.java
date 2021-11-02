f53fd20a3853104627944b312c8d34c543009cd2
xsue_glb <379045096@qq.com>
2021-11-02 23:54:31 +0800

    test commit push

--- src/main/java/com/node/domain/Student.java
+++ src/main/java/com/node/domain/Student.java
@@ -2,6 +2,23 @@ package com.node.domain;
 
 public class Student {
     private String name;
+    private String address;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                '}';
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
 
     public String getName() {
         return name;
